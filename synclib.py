0c0e34167335da281540cb983ef76894c9fa798e
Andrea Zonca <code@andreazonca.com>
2011-10-10 11:34:02 -0700

    fixed 20 bit jumps and ported write to pycfitsio

--- synclib.py
+++ synclib.py
@@ -1,7 +1,10 @@
+import exceptions
 import pyfits
 import pycfitsio
 import os
 import numpy as np
+from numpy.lib.recfunctions import append_fields
+from collections import OrderedDict
 
 REVCOUNTER_LABEL = {10:'REVCOUNTER_15GHZ', 15:'REVCOUNTER_10GHZ'}
 
@@ -24,8 +27,14 @@ def fix_counter_jumps_diff(d):
     d_fix = np.interp(t, t[fix], lin_d, left=lin_d[0]-1, right=lin_d[-1]+1)
     return d_fix.astype(np.int)
 
-def remove_reset(d):
+def remove_reset(d, offsetsci=None):
     """Gets longest time period between resets"""
+    # first check for 20bit jumps
+    jump20bit_indices, = np.where(np.logical_and(np.diff(d) < -2**20*.9, np.diff(d) > -2**20*1.1))
+    for j in jump20bit_indices:
+        d[j:] += 2**20
+    if not offsetsci is None:
+        d += 2**20 * np.ceil((offsetsci - d[0])/2**20)
     reset_indices, = np.where(np.diff(d) < -100000)
     sections_boundaries = np.concatenate([[0], reset_indices +1 ,[len(d)]])
     sections_lengths = np.diff(sections_boundaries)
@@ -39,6 +48,7 @@ class ServoSciSync(object):
         self.base_folder = base_folder
         self.day = day
         self.freq = freq
+        self.synched_data = OrderedDict()
         try:
             os.mkdir(os.path.join(self.base_folder, 'Level1'))
         except:
@@ -59,54 +69,42 @@ class ServoSciSync(object):
 
         self.counters = {}
         servo_count = self.servo[REVCOUNTER_LABEL[self.freq]].data.field(2)
-        servo_range = remove_reset(servo_count)
-        sci_count = self.data[0].array
+        sci_count = self.data['REV']
+        servo_range = remove_reset(servo_count, offsetsci=sci_count[0])
         #sci_range = remove_reset(sci_count)
         self.counters = {
                     'servo_range' : servo_range,
                     'servo' : fix_counter_jumps_diff(servo_count[servo_range]),
                     'sci_range' : None,
-                    'sci' : fix_counter_jumps(sci_count)
+                    'sci' : sci_count #fix_counter_jumps(sci_count)
                  } 
 
     def sync_clock(self):
-        self.clock = {}
-        self.data[0].array[:] = self.counters['sci']
-        self.clock =np.around(np.interp(self.counters['sci'], self.counters['servo'], self.servo[REVCOUNTER_LABEL[freq]].data.field('computerClock')[self.counters['servo_range']])).astype(np.int64)
-        newcol = pyfits.Column(
-            name = 'computerClock',
-            format = 'K',
-            array = self.clock
-            )
-        self.data.append(newcol)
+        self.synched_data['computerClock'] = np.around(np.interp(self.counters['sci'], self.counters['servo'], 
+                    self.servo[REVCOUNTER_LABEL[self.freq]].data.field('computerClock')[self.counters['servo_range']])).astype(np.int64)
+        append_fields(self.data, 'computerClock', self.synched_data['computerClock'])
 
     def sync_devices(self):
-            for device in self.devices:
-                ext = self.servo[device]
-                for col in ext.columns[1:]:
-                    newcol = pyfits.Column(
-                                name = '_'.join([ext.name, col.name]),
-                                format = col.format,
-                                array = np.interp(self.clock, ext.data.field('computerClock')[self.counters['servo_range']], ext.data.field(col.name)[self.counters['servo_range']])
-                                )
-                    self.data.append(newcol)
-            self.data.append(
-                    pyfits.Column(
-                        name='REVCHECK',
-                        format = 'K',
-                        array = np.interp(self.clock,
-                            self.servo[REVCOUNTER_LABEL].data.field('computerClock')[self.counters['servo_range']],
+        for device in self.devices:
+            ext = self.servo[device]
+            for col in ext.columns[1:]:
+                try:
+                    self.synched_data['_'.join([ext.name, col.name])] = np.interp(self.data['computerClock'], ext.data.field('computerClock')[self.counters['servo_range']], 
+                                ext.data.field(col.name)[self.counters['servo_range']])
+                except exceptions.ValueError:
+                    print('SKIPPING %s, no samples in range' % '_'.join([ext.name, col.name]))
+            self.synched_data['REVCHECK'] = np.interp(self.data['computerClock'],
+                            self.servo[REVCOUNTER_LABEL[self.freq]].data.field('computerClock')[self.counters['servo_range']],
                             self.servo[REVCOUNTER_LABEL[self.freq]].data.field('value')[self.counters['servo_range']]
                             )
-                        )
-                    )
-    
+
     def write(self):
         filename = os.path.join(self.base_folder, 'Level1','%s_%dGHz.fits' % (self.day, self.freq))
         print('Writing %s' % filename)
-        table = pyfits.new_table(self.data)
-        table.name = 'DATA'
-        table.writeto(filename,clobber=True)
+        f = pycfitsio.create(filename)
+        f.write_HDU('DATA', self.data)
+        f.write_HDU_dict('SERVO', self.synched_data)
+        f.close()
 
 
     def run(self):
